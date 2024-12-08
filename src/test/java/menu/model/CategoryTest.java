de698161cda95b23c2b86192c86a3d77a78f4d9b
7i.w__k <tizmfns1218@Naver.com>
2024-12-08 17:03:56 +0900

    test: 메서드 테스트

--- src/test/java/menu/model/CategoryTest.java
+++ src/test/java/menu/model/CategoryTest.java
@@ -1,15 +1,29 @@
 package menu.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CategoryTest {
+class CategoryTest {
 
     @Test
-    @DisplayName("카테고리 셔플 확인 테스트")
+    @DisplayName("같은 카테고리가 3개 이상 들어가면 true를 반환한다.")
     void test1() {
-        String food = Category.JPN.recommendFoodByRecommend();
+        List<Category> categories = List.of(Category.KOR, Category.KOR, Category.KOR, Category.CHN, Category.JPN);
 
-        System.out.println(food);
+        assertThat(Category.isNotValidWeeklyCategory(categories)).isTrue();
     }
+
+    @Test
+    @DisplayName("숫자에 따라 맞는 카테고리를 반환해야 한다.")
+    void test2() {
+        List<Category> expect = List.of(Category.JPN, Category.KOR, Category.CHN, Category.ASIAN, Category.WEST);
+
+        for (int number = 1; number <= 5; number++) {
+            assertThat(Category.getCategoryByNumber(number)).isEqualTo(expect.get(number - 1));
+        }
+    }
+
 }
---
+++ src/test/java/menu/model/CrewsTest.java
@@ -0,0 +1,23 @@
+package menu.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import menu.exception.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CrewsTest {
+
+    @ParameterizedTest
+    @DisplayName("크루원 수가 범위밖이면 에러 발생")
+    @ValueSource(strings = {"기욱", "기욱1,기욱2,기욱3,기욱4,기욱5,기욱6"})
+    void test1(String input) {
+        Crews crews = Crews.create();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> crews.addAll(input));
+
+        assertThat(exception.getMessage()).isEqualTo(ErrorMessage.EXCEED_CREW_SIZE.getMessage());
+
+    }
+}
